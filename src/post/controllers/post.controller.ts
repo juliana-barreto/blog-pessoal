a867e5efc107c00887058c6f606686f22fb3f65f
Juliana <89608026+juliana-barreto@users.noreply.github.com>
2026-03-04 18:10:16 -0300

    refactor Post module: enhance PostController and PostService for pagination and error handling

--- src/post/controllers/post.controller.ts
+++ src/post/controllers/post.controller.ts
@@ -1,17 +1,22 @@
-import { Controller, Get } from "@nestjs/common";
-import { PostService } from "../services/post.service";
-import { Post } from "../entities/post.entity";
+import {
+  Controller,
+  Get,
+  Query
+} from "@nestjs/common";
+import {PostService} from "../services/post.service";
+import {Post as PostEntity} from "../entities/post.entity";
 
 
 @Controller("/posts")
-export class PostController{
+export class PostController {
 
-    constructor(
-        private readonly postService: PostService
-      ){}
+  constructor(
+      private readonly postService: PostService
+  ) {
+  }
 
-    @Get()
-    findAll(): Promise<Post[]>{
-        return this.postService.findAll();
-    }
+  @Get()
+  findAll(@Query('page') page = 1, @Query('limit') limit = 10): Promise<PostEntity[]> {
+    return this.postService.findAll({page, limit});
+  }
 }
--- src/post/services/post.service.ts
+++ src/post/services/post.service.ts
@@ -1,17 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, InternalServerErrorException} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Post } from '../entities/post.entity';
+import { Post as PostEntity} from '../entities/post.entity';
 
 @Injectable()
 export class PostService {
 
   constructor(
-    @InjectRepository(Post)
-    private postRepository: Repository<Post>,
+    @InjectRepository(PostEntity)
+    private postRepository: Repository<PostEntity>,
   ) {}
 
-  async findAll(): Promise<Post[]> {
-    return this.postRepository.find();
+  async findAll(p: { page: number; limit: number }): Promise<PostEntity[]> {
+    try {
+      return this.postRepository.find();
+    } catch (error) {
+      throw new InternalServerErrorException('Error when fetching posts')
+    }
   }
 }
