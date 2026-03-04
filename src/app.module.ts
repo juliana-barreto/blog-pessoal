504fffad78a91f16d28015ebe0bddb0c994c1ab9
Juliana <89608026+juliana-barreto@users.noreply.github.com>
2026-03-04 13:58:26 -0300

    initialize NestJS application with basic structure and configuration

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostModule } from './post/post.module';
+import { Post } from './post/entities/post.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_blogpessoal',
+      entities: [Post],
+      synchronize: true,
+    }),
+    PostModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
